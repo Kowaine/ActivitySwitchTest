cd796c1180197da95a1aae23113b043d693d8a80
Kowaine <31977607+Kowaine@users.noreply.github.com>
2019-05-28 17:48:26 +0800

    添加两个活动之间的简单切换

--- app/src/main/java/com/example/activityswitchtest/SubActivity.java
+++ app/src/main/java/com/example/activityswitchtest/SubActivity.java
@@ -1,7 +1,9 @@
 package com.example.activityswitchtest;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class SubActivity extends AppCompatActivity {
 
@@ -9,5 +11,11 @@ public class SubActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sub);
+    }
+
+    public void onClick(View w)
+    {
+        Intent  intent = new Intent(SubActivity.this, MainActivity.class);
+        startActivity(intent);
     }
 }
